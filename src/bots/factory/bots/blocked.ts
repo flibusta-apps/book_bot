c8994aa2920d17714285a70defe4518a9508aaca
Kurbanov Bulat <kurbanovbul@gmail.com>
2022-01-24 22:39:34 +0300

    Fix pending and blocked bots

--- src/bots/factory/bots/blocked.ts
+++ src/bots/factory/bots/blocked.ts
@@ -2,9 +2,15 @@ import { Telegraf, Context } from 'telegraf';
 
 import { BotState } from '@/bots/manager';
 
+import env from '@/config';
+
 
 export async function createBlockedBot(token: string, state: BotState): Promise<Telegraf> {
-    const bot = new Telegraf(token);
+    const bot = new Telegraf(token, {
+        telegram: {
+            apiRoot: env.TELEGRAM_BOT_API_ROOT,
+        }
+    });
 
     await bot.telegram.deleteMyCommands();
 
