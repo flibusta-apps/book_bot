1885549a7528113eef92ae891832ade1a49807fd
Bulat Kurbanov <kurbanovbul@gmail.com>
2024-08-23 11:11:37 +0200

    Fix search

--- src/bots/approved_bot/modules/search/utils.rs
+++ src/bots/approved_bot/modules/search/utils.rs
@@ -6,9 +6,14 @@ pub fn get_query(cq: CallbackQuery) -> Option<String> {
         Some(message) => {
             match message {
                 MaybeInaccessibleMessage::Regular(message) => {
-                    message
-                        .text()
-                        .map_or(None, |text| Some(text.replace(['/', '&', '?'], "")))
+                    match message.reply_to_message() {
+                        Some(reply_to_message) => {
+                            reply_to_message
+                                .text()
+                                .map(|text| text.replace(['/', '&', '?'], ""))
+                        }
+                        None => None,
+                    }
                 }
                 MaybeInaccessibleMessage::Inaccessible(_) => None,
             }
