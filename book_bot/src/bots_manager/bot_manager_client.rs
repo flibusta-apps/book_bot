012d3083210db3b7dcdfc9d7315a6589c006939d
Bulat Kurbanov <kurbanovbul@gmail.com>
2026-07-08 14:39:04 +0200

    fix(bots_manager): check manager API response status before parsing
    get_bots on a 401/500 previously surfaced as a confusing JSON
    deserialization error; delete_bot returned Ok(()) for any status,
    treating a failed delete as a success.

--- book_bot/src/bots_manager/bot_manager_client.rs
+++ book_bot/src/bots_manager/bot_manager_client.rs
@@ -32,12 +32,13 @@ pub async fn get_bots() -> Result<Vec<BotData>, reqwest::Error> {
         .get(&config::CONFIG.manager_url)
         .header("Authorization", &config::CONFIG.manager_api_key)
         .send()
-        .await;
+        .await?;
 
-    match response {
-        Ok(v) => v.json::<Vec<BotData>>().await,
-        Err(err) => Err(err),
-    }
+    parse_bots_response(response).await
+}
+
+async fn parse_bots_response(response: reqwest::Response) -> Result<Vec<BotData>, reqwest::Error> {
+    response.error_for_status()?.json::<Vec<BotData>>().await
 }
 
 pub async fn delete_bot(id: u32) -> Result<(), reqwest::Error> {
@@ -45,10 +46,55 @@ pub async fn delete_bot(id: u32) -> Result<(), reqwest::Error> {
         .delete(format!("{}/{}/", config::CONFIG.manager_url, id))
         .header("Authorization", &config::CONFIG.manager_api_key)
         .send()
-        .await;
+        .await?;
 
-    match response {
-        Ok(_) => Ok(()),
-        Err(err) => Err(err),
+    check_delete_response(response)
+}
+
+fn check_delete_response(response: reqwest::Response) -> Result<(), reqwest::Error> {
+    response.error_for_status()?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn response_with_status(status: u16) -> reqwest::Response {
+        let http_response = http::Response::builder()
+            .status(status)
+            .body(Vec::<u8>::new())
+            .unwrap();
+        reqwest::Response::from(http_response)
+    }
+
+    #[tokio::test]
+    async fn parse_bots_response_errors_on_401() {
+        let response = response_with_status(401);
+        assert!(parse_bots_response(response).await.is_err());
+    }
+
+    #[tokio::test]
+    async fn parse_bots_response_parses_valid_json() {
+        let http_response = http::Response::builder()
+            .status(200)
+            .body(br#"[{"id":1,"token":"abc","cache":"cache"}]"#.to_vec())
+            .unwrap();
+        let response = reqwest::Response::from(http_response);
+
+        let bots = parse_bots_response(response).await.unwrap();
+        assert_eq!(bots.len(), 1);
+        assert_eq!(bots[0].id, 1);
+        assert_eq!(bots[0].cache, BotCache::Cache);
+    }
+
+    #[test]
+    fn check_delete_response_errors_on_500() {
+        assert!(check_delete_response(response_with_status(500)).is_err());
+    }
+
+    #[test]
+    fn check_delete_response_ok_on_200() {
+        assert!(check_delete_response(response_with_status(200)).is_ok());
     }
 }
